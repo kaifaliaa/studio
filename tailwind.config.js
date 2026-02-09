01d02cf01240cf64e1f04f665c4c9cbab4fbde50
kaifaliaa <abdulkadir706065@gmail.com>
2026-02-09 18:33:42 +0530

    Update color configuration in tailwind.config.js
    Removed custom purple color palette and added primary, secondary, accent, highlight, text, and background colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -10,16 +11,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        purple: {
-          50: '#faf5ff',
-          100: '#f3e8ff',
-          200: '#e9d5ff',
-          300: '#d8b4fe',
-          400: '#c084fc',
-          500: '#a855f7',
-          600: '#9333ea',
-          700: '#6b21a8',
-        }
+        primary: "#1A202C",
+        secondary: "#2D3748",
+        accent: "#4A5568",
+        highlight: "#718096",
+        text: "#E2E8F0",
+        background: "#F7FAFC",
       }
     },
   },
